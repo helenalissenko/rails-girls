48cd3902c73508d6de519b10a45fb43c6b4f89e2
helenalissenko <helena.lissenko@gmail.com>
2014-04-26 12:00:54 +0300

    commenting added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426074135) do
+ActiveRecord::Schema.define(version: 20140426085634) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ideas", force: true do |t|
     t.string   "name"
